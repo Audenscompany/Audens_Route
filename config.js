fc667ad541dd926a84c84f0228909e64d79df4a2
Audenscompany <audenscompany@gmail.com>
2026-07-12 14:11:27 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+window.AUDENS_CONFIG = {
+  apiUrl: "https://audens-route-api-452139275816.southamerica-east1.run.app",
+  firebase: {
+    apiKey: "AIzaSyALkNr5ufB8DQ54DJFHAX7-obcg8N7SM8o",
+    authDomain: "audens-route.firebaseapp.com",
+    projectId: "audens-route",
+    storageBucket: "audens-route.firebasestorage.app",
+    messagingSenderId: "406257478885",
+    appId: "1:406257478885:web:b2eb532fd23f575e34780c",
+    measurementId: "G-9JL27K3LTE"
+  }
+};
+window.AUDENS_HAS_FIREBASE = !!(window.AUDENS_CONFIG.firebase && window.AUDENS_CONFIG.firebase.apiKey);
